58df4e28746767fa14905533eeb7b7433266066f
NRpatel002 <dev.patel@innovationcharter.org>
2024-10-08 13:14:34 +0000

    Dp

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,10 @@
 import "../styles/styles.css";
 import "@picocss/pico/css/pico.min.css"; 
-// Add imports here
 import Aos from "aos";
 import "aos/dist/aos.css";
 
-console.log("What?");
-console.log("Hello world, from main.js!");
+// Initialize AOS (Animate On Scroll) library
 if (typeof window !== "undefined") {
-  // Put code that runs ONLY in the browser here -- this is most likely where
-Aos.init();
-
-  // MOST of your code should go.
-  //
-  // If you see a SSR error, try moving your code here!
-  console.log("Hello world, browser :-)");
+  Aos.init();
+  console.log("Hello world, from main.js!");
 }
