224e6ca5e1798a651cb87b6d15700229e571c682
NRpatel002 <dev.patel@innovationcharter.org>
2024-10-10 13:03:33 +0000

    Dp

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,9 @@
 import "../styles/styles.css";
-import "@picocss/pico/css/pico.min.css"; 
-import aos from "aos";
+//import "@picocss/pico/css/pico.min.css"; 
 import Aos from "aos";
 import "aos/dist/aos.css";
 
-// Initialize AOS (Animate On Scroll) library
+ //Initialize AOS (Animate On Scroll) library
 if (typeof window !== "undefined") {
   Aos.init();
   console.log("Hello world, from main.js!");
